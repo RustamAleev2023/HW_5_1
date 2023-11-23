5c6c7be88b32611095cc4884a888353fa3948b22
RustamAleev2023 <ridermix@yandex.ru>
2023-11-23 10:17:46 +0200

    Task2 done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import java.util.Random;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        task1();
+//        task1();
+        task2();
     }
+
     //Task1
-    public static void task1(){
+    public static void task1() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку");
         String string = scanner.nextLine();
@@ -16,16 +19,34 @@ public class Main {
         int counter = 0;
 
         for (int i = 0; i < string.length(); i++) {
-            if(string.charAt(i) == symbol){
+            if (string.charAt(i) == symbol) {
                 System.out.println(i);
                 counter++;
             }
         }
-        if(counter == 0){
+        if (counter == 0) {
             System.out.println("Совпадений не найдено");
         } else {
             System.out.println("Кол-во совпадений = " + counter);
         }
+    }
 
+    //Task2
+    public static void task2() {
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 1000; i++) {
+            stringBuilder.append(String.valueOf(random.nextInt(9) + 1));
+        }
+        String randomNum = stringBuilder.toString();
+        System.out.println(randomNum);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер позиции с 1 до 1000");
+        int n = scanner.nextInt();
+        if (n > 0 && n <= 1000) {
+            System.out.println(randomNum.charAt(n - 1));
+        } else {
+            System.out.println("вы вышли за пределы диапазона");
+        }
     }
 }
